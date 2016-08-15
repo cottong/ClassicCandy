ed9782fdb7a87bf6ff5f26464cc6d8b0e69015c1
Jamila Carrington Smith <j.carringtonsmith@gmail.com>
2016-08-15 13:25:45 -0400

    testing

---
+++ src/paymentTypes/Check.java
@@ -0,0 +1,36 @@
+package paymentTypes;
+
+public class Check extends Payment {
+
+	private int checkNumber;
+
+	
+	public int getCheckNumber() {
+		return checkNumber;
+	}
+	public void setCheckNumber(int checkNumber) {
+		this.checkNumber = checkNumber;
+	}
+	
+	/*@Override
+	public double calcSubtotal() {
+		double subtotal = 0.0;
+		subtotal = getPrice() * getQuantity();
+		return subtotal;
+	}
+
+	@Override
+	public double calcGrandTotal() {
+		double grandTotal = 0.0;
+		grandTotal = getPrice() * getQuantity() * (1 + tax);
+		return grandTotal;
+	}
+
+	@Override
+	public double calcSalesTax() {
+		double salesTax = calcSubtotal() * tax;
+		return salesTax;
+	}*/
+		
+		
+}
