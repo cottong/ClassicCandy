8de17bced60cad0b81313ea9056c6fc34c908805
Jamila Carrington Smith <j.carringtonsmith@gmail.com>
2016-08-15 17:00:54 -0400

    main method push 8/15 5:00pm

--- src/posMain/POSApp.java
+++ src/posMain/POSApp.java
@@ -1,11 +1,20 @@
 package posMain;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
+//import java.paymentTypes.*;
+import paymentTypes.CandyWriteFile;
+import paymentTypes.Payment;
 
 public class POSApp {
 
 	public static void main(String[] args) {
 
+		Scanner scan1 = new Scanner (System.in);
+		
 		ArrayList<ProductClass> list = new ArrayList<ProductClass>();
 		list.add(new ProductClass("m&m", "chocolate", "chocolate with candy coating", 1.89));
 		list.add(new ProductClass("Hershey's Kiss", "chocolate", "small delicious chocolate", 1.89));
@@ -21,6 +30,32 @@ public class POSApp {
 		list.add(new ProductClass("Three Musketeers", "chocolate", "chocolate covered nugget", 1.89));
 		list.add(new ProductClass("Candy Corn", "chewy candy", "small halloween candy", 1.89));
 
+		
+		//greet the user
+		System.out.println("Welcome to the Classic Candy Store.\nHere are the candies you can choose from: ");
+		
+		//read the text file and prints to the console
+		Path filePath = Paths.get("CandyData.txt");
+		File myCandyFile = filePath.toFile();
+		CandyWriteFile.readFile(myCandyFile);
+		
+		//prompt the user choose an item
+		int candyNumber = scan1.nextInt();
+		scan1.nextLine();
+		String candyName = list.get(candyNumber - 1).getName();
+		//System.out.println(candyName);
+		
+		//prompt the user for a quantity
+		System.out.println("How many " + candyName + " do you want to buy today?");
+		int quantity = scan1.nextInt();
+		scan1.nextLine();
+		//Payment.setQuantity(quantity);
+		
+		//output subtotal
+		
+
+		System.out.println();
+		
 	}
 
 }
