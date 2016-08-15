dfd52a5404f9c199071480a032abda077839c91b
Kristen Nader <kjnader@gmail.com>
2016-08-15 15:54:46 -0400

    Added Product Class and updated POSApp

--- src/posMain/POSApp.java
+++ src/posMain/POSApp.java
@@ -1,10 +1,24 @@
 package posMain;
 
+import java.util.ArrayList;
+
 public class POSApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<ProductClass> list = new ArrayList<ProductClass>();
+		list.add(new ProductClass("m&m", "chocolate", "chocolate with candy coating", 1.89));
+		list.add(new ProductClass("Hershey's Kiss", "chocolate", "small delicious chocolate", 1.89));
+		list.add(new ProductClass("Snickers", "chocolate", "chocolate with nuts and caramel", 1.89));
+		list.add(new ProductClass("Twizzlers", "chewy candy", "red licorice", 1.50));
+		list.add(new ProductClass("Reese's peanut butter cups", "chocolate", "chocolate with peanut butter", 1.89));
+		list.add(new ProductClass("Kit-Kat", "chocolate", "chocolate covered waffers", 1.89));
+		list.add(new ProductClass("Gummi bears/worms/ etc", "chewy candy", "gummy yummy candy", 1.89));
+		list.add(new ProductClass("Butterfinger", "chocolate", "chocolate covered toffee", 1.89));
+		list.add(new ProductClass("Twix", "chocolate", "chocolate, caramel covered cookie", 1.89));
+		list.add(new ProductClass("Hershey Bar", "chocolate", "smooth milk chocolate", 1.89));
+		list.add(new ProductClass("Jelly Belly", "chewy candy", "small jelly beans", 1.89));
+		list.add(new ProductClass("Three Musketeers", "chocolate", "chocolate covered nugget", 1.89));
+		list.add(new ProductClass("Candy Corn", "chewy candy", "small halloween candy", 1.89));
 	}
 
 }
---
+++ src/posMain/ProductClass.java
@@ -0,0 +1,47 @@
+package posMain;
+
+import java.util.ArrayList;
+
+public class ProductClass {
+	
+	private String name;
+	private String category;
+	private String description;
+	private double price;
+	
+	public ProductClass(String name, String category, String description, double price) {
+		
+		setName(name);
+		setCategory(category);
+		setDescription(description);
+		setPrice(price);
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getName(String name) {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+	
+	
+}
