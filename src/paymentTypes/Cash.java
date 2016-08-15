ce2cd879248e51fc019617a02f9284f05d2ed199
Gerrae Cotton <gerrae.cotton@gmail.com>
2016-08-15 13:24:45 -0400

    Add Cash Class

---
+++ src/paymentTypes/Cash.java
@@ -0,0 +1,27 @@
+package paymentTypes;
+
+import java.util.Scanner;
+
+public class Cash extends Payment {
+	private double change;
+	private double amountTendered;
+
+
+	public double getAmountTendered() {
+		return amountTendered;
+	}
+	public void setAmountTendered(double amountTendered) {
+		this.amountTendered = amountTendered;
+	}
+
+	public double calcChange(){
+		System.out.println("How much are you paying with today?");
+		Scanner scan1 = new Scanner(System.in);
+		double userInput = scan1.nextDouble();
+		scan1.nextLine();
+		setAmountTendered(userInput);
+		change = getAmountTendered() - calcGrandTotal();
+		return change;
+	}
+
+}
