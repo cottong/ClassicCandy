98838b7686357ca1b2257ca3f19812641b7f9dfe
Kristen Nader <kjnader@gmail.com>
2016-08-17 15:18:22 -0400

    Added more candy

--- src/posMain/POSApp.java
+++ src/posMain/POSApp.java
@@ -26,57 +26,107 @@ public class POSApp {
 
 		ArrayList<ProductClass> list = new ArrayList<ProductClass>();
 
-		list.add(new ProductClass("m&m", "chocolate",
+		list.add(new ProductClass("m&m", "chocolate", "chocolate with candy coating", 1.00));
 
-				"chocolate with candy coating", 1.00));
+		list.add(new ProductClass("Hershey's Kiss", "chocolate", "small delicious chocolate", 2.00));
 
-		list.add(new ProductClass("Hershey's Kiss", "chocolate",
+		list.add(new ProductClass("Snickers", "chocolate", "chocolate with nuts and caramel", 1.89));
 
-				"small delicious chocolate", 2.00));
+		list.add(new ProductClass("Twizzlers", "chewy candy", "red licorice", 1.50));
 
-		list.add(new ProductClass("Snickers", "chocolate",
+		list.add(new ProductClass("Reese's peanut butter cups", "chocolate", "chocolate with peanut butter", 1.89));
 
-				"chocolate with nuts and caramel", 1.89));
+		list.add(new ProductClass("Kit-Kat", "candy bar", "chocolate covered waffers", 1.89));
 
-		list.add(new ProductClass("Twizzlers", "chewy candy", "red licorice",
+		list.add(new ProductClass("Gummi bears/worms/ etc", "chewy candy", "gummy yummy candy", 1.89));
 
-				1.50));
+		list.add(new ProductClass("Butterfinger", "candy bar", "chocolate covered toffee", 1.89));
 
-		list.add(new ProductClass("Reese's peanut butter cups", "chocolate",
+		list.add(new ProductClass("Twix", "candy bar", "chocolate, caramel covered cookie", 1.89));
 
-				"chocolate with peanut butter", 1.89));
+		list.add(new ProductClass("Hershey Bar", "candy bar", "smooth milk chocolate", 1.89));
 
-		list.add(new ProductClass("Kit-Kat", "chocolate",
+		list.add(new ProductClass("Jelly Belly", "chewy candy", "small jelly beans", 1.89));
 
-				"chocolate covered waffers", 1.89));
+		list.add(new ProductClass("Three Musketeers", "candy bar", "chocolate covered nugget", 1.89));
 
-		list.add(new ProductClass("Gummi bears/worms/ etc", "chewy candy",
-
-				"gummy yummy candy", 1.89));
-
-		list.add(new ProductClass("Butterfinger", "chocolate",
-
-				"chocolate covered toffee", 1.89));
-
-		list.add(new ProductClass("Twix", "chocolate",
-
-				"chocolate, caramel covered cookie", 1.89));
-
-		list.add(new ProductClass("Hershey Bar", "chocolate",
-
-				"smooth milk chocolate", 1.89));
-
-		list.add(new ProductClass("Jelly Belly", "chewy candy",
-
-				"small jelly beans", 1.89));
-
-		list.add(new ProductClass("Three Musketeers", "chocolate",
-
-				"chocolate covered nugget", 1.89));
-
-		list.add(new ProductClass("Candy Corn", "chewy candy",
-
-				"small halloween candy", 1.89));
+		list.add(new ProductClass("Candy Corn", "chewy candy", "small halloween candy", 1.89));
+		
+		list.add(new ProductClass("York Peppermint Patties", "chocolate", "a refreshing mix minty chocolate covered yumminess", 2.85));
+		
+		list.add(new ProductClass("Tootsie Rolls/Tootsie Roll Pops", "hard candy", "Can you get to the end of a tootsie roll pop?  Hard candy with chocolate goodness", 1.50));
+		
+		list.add(new ProductClass("Skittles", "chewy candy", "A chewy, hard at first, sweet candy", 2.50));
+		
+		list.add(new ProductClass("Cadbury eggs", "chocolate", "chocolate covered gueyness", 1.29));
+		
+		list.add(new ProductClass("Milky Way", "candy bar", "milk chocolate with caramel and nugget", 1.89));
+		
+		list.add(new ProductClass("Swedish Fish", "chewy", "old school favorite", .25));
+		
+		list.add(new ProductClass("Cotton Candy", "strait sugar", "melts in your mouth to sugar", 2.50));
+		
+		list.add(new ProductClass("Junior Mints", "chocolate", "chocolate covered mint", 1.89));
+		
+		list.add(new ProductClass("Reese's pieces", "chocolate", "peanut butter pieces", 1.89));
+		
+		list.add(new ProductClass("Peeps", "strait sugar", "fun candy that has a face", 1.29));
+		
+		list.add(new ProductClass("Payday", "candy bar", "peanuts and caramel", 1.89));
+		
+		list.add(new ProductClass("Baby Ruth", "candy bar", "chocolate covered nuts", 1.89));
+		
+		list.add(new ProductClass("Starburst", "chewy", "bursts of fruit flavor", 1.89));
+		
+		list.add(new ProductClass("Atomic Fireballs", "hardcandy", "not for the weak, hot", 1.79));
+		
+		list.add(new ProductClass("Sour Patch Kids", "chewy", "first they are sweet then they're sour", 1.89));
+		
+		list.add(new ProductClass("Almond Joy", "candy bar", "coconuts covered in chocolate", 1.49));
+		
+		list.add(new ProductClass("Blow pops", "hard candy", "sucker with gum inside", 1.89));
+		
+		list.add(new ProductClass("Pixie sticks", "strait sugar", "flavored sugar in a stick", 1.39));
+		
+		list.add(new ProductClass("Milk duds", "chocolate", "chocolate and caramel bits", 1.79));
+		
+		list.add(new ProductClass("Pez", "strait sugar", "cool dispensers and little candies", 1.89));
+		
+		list.add(new ProductClass("Smarties", "strait sugar", "Small candies that make you smarter", 1.89));
+		
+		list.add(new ProductClass("Mr. Goodbar", "candy bar", "peanuts and chocolate", 1.89));
+		
+		list.add(new ProductClass("Toblerone", "candy bar", "peanuts and caramel", 1.89));
+		
+		list.add(new ProductClass("Jolly Ranchers", "hard candy", "intense sweetness", 1.89));
+		
+		list.add(new ProductClass("Red Vines", "chewy", "licorice", 1.89));
+		
+		list.add(new ProductClass("Salt Water Taffy", "chewy", "taffy", 1.59));
+		
+		list.add(new ProductClass("Candy cigarettes", "hard candy", "sugar stick", 1.89));
+		
+		list.add(new ProductClass("Pocky", "chewy", "sweet candy", 1.89));
+		
+		list.add(new ProductClass("Nerds", "strait sugar", "a favorite for parties", 1.89));
+		
+		list.add(new ProductClass("Necco Wafers", "strait sugar", "a rollerskating favorite", 1.89));
+		
+		list.add(new ProductClass("Pop rocks", "strait sugar", "it will pop in your mouth", 1.89));
+		
+		list.add(new ProductClass("Sugar daddy", "chewy", "caramel covered with chocolate", 1.59));
+		
+		list.add(new ProductClass("Wax lips", "wax candy", "novelty candy", 1.89));
+		
+		list.add(new ProductClass("Circus peanuts", "peanuts", "peanuts", 1.89));
+		
+		list.add(new ProductClass("Nik-L-Nips", "wax candy", "wax bottles", 1.89));
+		
+		list.add(new ProductClass("Werthers Original", "hard candy", "hard caramel", 1.89));
+		
+		
+		
+		
 
 		// greet the user
 
